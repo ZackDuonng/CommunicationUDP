9460c8ff3fac66ea7a33745b54882e6419fe9e00
Tai Tran <thanhtait4025@gmail.com>
2025-10-15 10:17:09 +0700

    feat: Add build folder of CMake after running to create executive files

---
+++ build/CMakeFiles/udp_server.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/thanhtai/GREMSY/udp_server_update.cpp" "/home/thanhtai/GREMSY/build/CMakeFiles/udp_server.dir/udp_server_update.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
