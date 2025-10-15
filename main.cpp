2e876899419784819cb5e153aba49045bbf39da3
Tai Tran <thanhtait4025@gmail.com>
2025-10-15 16:42:39 +0700

    chore: Add library

--- main.cpp
+++ main.cpp
@@ -1,18 +1,17 @@
-#include "include/UDPServer.h"
+#include "UDPServer.h" 
+#include "test_file.h"   
 #include <iostream>
 
 #define PORT 8080
 
 int main() {
     try {
-
+        print();
         UDPServer server(PORT);
         server.start();
-
     } catch (const std::exception& e) {
         std::cerr << "Error: " << e.what() << std::endl;
         return 1;
     }
-
     return 0;
 }
