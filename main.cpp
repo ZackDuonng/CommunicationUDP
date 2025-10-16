060f8da31d2db43543bf242163800a187c3823f5
DuongDang <duongdang4869@gmail.com>
2025-10-16 10:21:27 +0700

    Reuse libary

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "CommunicationUDP/include/UDPServer.h" 
+#include "CommunicationUDP/include/test_file.h"   
+#include <iostream>
+
+#define PORT 8080
+
+int main() {
+    try {
+        print();
+        UDPServer server(PORT);
+        server.start();
+    } catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return 1;
+    }
+    return 0;
+}
