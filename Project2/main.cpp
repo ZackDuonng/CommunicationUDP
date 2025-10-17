b3e1fe4d03970f93dd5c07d14e389a0191116e34
Tai Tran <thanhtait4025@gmail.com>
2025-10-17 16:19:47 +0700

    feat: Add print line in main.cpp

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -5,6 +5,7 @@
 #define PORT 8080
 
 int main() {
+    std::cout<<"Chuong trinh dang chay ne !!!"<<std::endl;
     int *ptr  = new int[10];
     std::cout << "Address of allocated memory: " << ptr[11] << std::endl;
     return 0;
